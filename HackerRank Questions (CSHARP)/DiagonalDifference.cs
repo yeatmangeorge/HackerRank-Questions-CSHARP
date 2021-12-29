9b55376e837b869b46d4a6b5c7cf540c80044b77
yeatmangeorge <70824909+yeatmangeorge@users.noreply.github.com>
2021-12-29 19:42:36 +0000

    Add project files.

---
+++ HackerRank Questions (CSHARP)/DiagonalDifference.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Questions__CSHARP_
+{
+	internal class Result
+	{
+
+		public static int diagonalDifference(List<List<int>> arr)
+		{
+			int sumOfLeftToRight = 0;
+			int sumOfRightToLeft = 0;
+
+			for(int i = 0; i < arr.Count; i++)
+            {
+				sumOfLeftToRight += arr[i][i];
+            }
+
+			int x = 0;
+
+			for(int y = arr.Count-1;y>=0; y--)
+            {
+				sumOfRightToLeft += arr[x][y];
+				x++;
+            }
+			return Math.Abs(sumOfLeftToRight-sumOfRightToLeft);
+		}
+	}
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+			List<int> a = new List<int>{ 1,2,3};
+			List<int> b = new List<int>{ 4,5,6};
+			List<int> c = new List<int>{ 9,8,10};
+
+			List<List<int>> test = new List<List<int>> { a,b,c};
+
+			Console.WriteLine(Result.diagonalDifference(test));
+		}
+    }
+}
